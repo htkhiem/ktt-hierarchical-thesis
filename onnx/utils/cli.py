334d81f7de4858a3b4b7d84c153bddd9ec841f77
Huynh Thien Khiem <42716602+htkhiem@users.noreply.github.com>
2022-03-29 12:44:59 +0700

    Add missing CLI variables

---
+++ onnx/utils/cli.py
@@ -0,0 +1,16 @@
+"""Small utilities to aid in printing to console."""
+
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+DARKCYAN = '\033[36m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+PLAIN = '\033[0m'
+
+
+if __name__ == '__main__':
+    pass
