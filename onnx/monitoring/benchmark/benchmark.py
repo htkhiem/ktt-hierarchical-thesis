550f2b729ccbd2a509a2e2cc13967c48de441d5c
Huynh Thien Khiem <42716602+htkhiem@users.noreply.github.com>
2022-03-31 09:11:52 +0700

    Add Locust benchmarking and Prometheus preset

---
+++ onnx/monitoring/benchmark/benchmark.py
@@ -0,0 +1,31 @@
+"""
+Stage and run a Locust benchmark using strings from a test set.
+"""
+import os
+import json
+import pandas as pd
+import click
+
+
+@click.command()
+@click.argument('dataset_name')
+def main(dataset_name):
+    """Extract string inputs from a dataset and use it to benchmark a server.
+
+    Pass to it the name of an intermediate dataset to use strings from there.
+    """
+    path = '../../datasets/{}/test.parquet'.format(dataset_name)
+
+    test_set = pd.read_parquet(path)
+
+    json_content = {
+        'strings': test_set['name'].tolist()
+    }
+
+    with open('text-strings.json', 'w') as jsonfile:
+        json.dump(json_content, jsonfile)
+
+    os.system('locust')
+
+if __name__ == '__main__':
+    main()
